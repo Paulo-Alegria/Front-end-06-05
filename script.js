18cf02704eb4c219de380a4f62790d3b5519734a
Paulo <pauloalegriadiaz6@gmail.com>
2025-05-06 17:26:47 -0400

    update

---
+++ script.js
@@ -0,0 +1,29 @@
+const students = []
+// funciona como lita
+document.getElementById("studentform").addEventListener("submit",function(e){
+ e.preventDefault();
+ const name = document.getElementById("name").value.trim();
+ const lastname = document.getElementById("lastname").value.trim();
+ const grade = parseFloat(document.getElementById("grade").value);
+
+ if(grade<1 || grade>7 ||!name || !lastname || isNaN(grade)){
+    alert("Error al ingresar los datos")
+    return
+ }
+ const student = {name,lastname,grade} 
+ students.push(student);
+ addStudentsToTable(student);
+  
+  this.reset()
+
+});
+const tablebody= document.querySelector("#studenttable tbody");
+function addStudentsToTable(student){
+    const row= document.createElement("tr");
+    row.innerHTML=`
+    <td>${student.name}</td>
+    <td>${student.lastname}</td>
+    <td>${student.grade}</td>
+    `;
+ tablebody.appendChild(row);
+}
